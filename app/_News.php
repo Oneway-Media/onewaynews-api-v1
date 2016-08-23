fd3301b6baf2dcafd34d178eace2305eabc5a4b9
Simeon <smokc2002@gmail.com>
2016-08-23 17:23:06 +0700

    /news-item/:slug|:id/:fields

--- app/_News.php
+++ app/_News.php
@@ -322,7 +322,7 @@ class News {
         // Switch $id to ID from slug
         if( !is_numeric($id) ) {
             // get posst from slug
-            $post = get_page_by_path($id, OBJECT, 'news');
+            $post = get_page_by_path($id, OBJECT, 'post');
             
         } else {
             //get post from ID
@@ -360,8 +360,8 @@ class News {
                     //$p['cover'] = $img['1'];
                     $p['cover'] = wp_get_attachment_image_src( get_post_thumbnail_id( $p['id'] ), 'full' )[0];
                     $p['view'] = intval(get_post_meta( $p['id'], '_count-views_all', true ));
-                    $p['like'] = intval(get_post_meta( $p['id'], 'oneway_like', true ));
-                    $p['share'] = intval(get_post_meta( $p['id'], 'oneway_share', true ));
+                    $p['like'] = intval(get_post_meta( $p['id'], 'news.oneway_like', true ));
+                    $p['share'] = intval(get_post_meta( $p['id'], 'news.oneway_share', true ));
                     $p['comment'] = intval(wp_count_comments($p['id'])->approved);
                     
                     // Filter the content
@@ -376,7 +376,7 @@ class News {
                 return [];
             }
         } else if ($fields == 'extra')  {
-            $cmts = get_comments( 'post_id='.$id);
+            $cmts = get_comments( 'ID='.$id);
             $extra_fields = [
                 'id' => 'comment_ID',
                 'author_name' => 'comment_author',
