7345e2e1d8bc3969d96ead001180bcada5f20b46
Grace Pham <zenna.pham@gmail.com>
2016-09-06 09:49:45 +0700

    renamed thumnail var

--- app/_News.php
+++ app/_News.php
@@ -140,7 +140,7 @@ class News {
         if(count($pre) > 0) {
             foreach($pre as $p) {
                 $p['thumbnail'] =  wp_get_attachment_image_src( get_post_thumbnail_id( $p['id'] ), 'thumbnail' )[0];
-                $p['thumbnail-med'] =  wp_get_attachment_image_src( get_post_thumbnail_id( $p['id'] ), 'medium' )[0];
+                $p['thumbnail_med'] =  wp_get_attachment_image_src( get_post_thumbnail_id( $p['id'] ), 'medium' )[0];
                 $p['permalink'] = get_permalink($p['id']);
                 $p['cover'] =  wp_get_attachment_image_src( get_post_thumbnail_id( $p['id'] ), 'large' )[0];
                 $p['view'] = intval(get_post_meta( $p['id'], '_count-views_all', true ));
